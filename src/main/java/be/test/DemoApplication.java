c17baab56c765ca23990e067867cf6561f6fdb22
Tom De Dobbeleer <tom.dedobbeleer@kuleuven.be>
2016-11-09 16:58:29 +0100

    woops

--- src/main/java/be/test/DemoApplication.java
+++ src/main/java/be/test/DemoApplication.java
@@ -1,12 +1,38 @@
 package be.test;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
+@ComponentScan
+@EnableAutoConfiguration
 @SpringBootApplication
-public class DemoApplication {
+public class DemoApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(DemoApplication.class);
+	}
 }
+
+@RestController
+class TestController {
+	@RequestMapping("/hello")
+	@ResponseBody
+	public String test() {
+		return "test";
+	}
+}
+
+
